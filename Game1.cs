50023bbc9de16abbe3ded7a889c0ec899d51652d
Ethan Jones <339467243@GoTVDSB.ca>
2024-05-16 12:59:57 -0400

    FINISHED ATLAST

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,7 @@ namespace Mono_1_assingment
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         Texture2D _discoText, _zeusText, _androidText, _alienText, _scaredManText;
+        Rectangle background;
         
 
         public Game1()
@@ -23,17 +24,21 @@ namespace Mono_1_assingment
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.ApplyChanges();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            background = new Rectangle(0, 0, 800, 500);
             _alienText = Content.Load<Texture2D>("alien pc");
-            _zeusText = Content.Load<Texture2D>("Zeus action fig")
-            _discoText = Content.Load<Texture2D>("disco party")
-            _scaredManText = Content.Load<Texture2D>("scared man")
-            _androidText = Content.Load<Texture2D>("android")
-            _
+            _zeusText = Content.Load<Texture2D>("Zeus action fig");
+            _discoText = Content.Load<Texture2D>("disco party");
+            _scaredManText = Content.Load<Texture2D>("scared man");
+            _androidText = Content.Load<Texture2D>("android");
+            
             // TODO: use this.Content to load your game content here
         }
 
@@ -52,7 +57,13 @@ namespace Mono_1_assingment
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_discoText,background, Color.White);
+            _spriteBatch.Draw(_zeusText, new Vector2(300,250), Color.White);
+            _spriteBatch.Draw(_alienText, new Vector2(100,405), Color.White);
+            _spriteBatch.Draw(_androidText, new Vector2(320,390),Color.White);
+            _spriteBatch.Draw(_scaredManText, new Vector2(650, 350), Color.White);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
